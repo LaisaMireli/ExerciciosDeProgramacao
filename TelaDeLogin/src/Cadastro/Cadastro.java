e37b116fed2c782c8687d38d8d48f04896f60d21
LaisaMireli <laisamireli57@alu.ufc.br>
2023-06-06 19:04:50 -0300

    Interface Gráfica

--- TelaDeLogin/src/Cadastro/Cadastro.java
+++ TelaDeLogin/src/Cadastro/Cadastro.java
@@ -1,24 +1,39 @@
 package Cadastro;
 
 import java.awt.GridLayout;
+import java.awt.Button;
+import java.awt.Dimension;
+
+import javax.swing.BorderFactory;
+import javax.swing.JButton;
 
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
+import javax.swing.*;
 
 public class Cadastro extends JPanel {
 	
 	
 	JPanel CadastroPainel;
+	JPanel ListarPainel;
+	JPanel AtualizarPainel;
+	JPanel DeletarPainel;
+	JTable tabela;
+	JScrollPane barraRolagem;
 
     public Cadastro() {
-
-    	this.add(cadastrar());
+    	
+    	this.add(cadastrarUsuário());
+    	this.add(listarUsuário());
+    	this.add(AtualizarUsuario());
+    	this.add(DeletarUsuario());
     }
 
-	public JPanel cadastrar() {
+	public JPanel cadastrarUsuário() {
     	
-    	CadastroPainel = new JPanel();    	
+    	CadastroPainel = new JPanel();    
+    	CadastroPainel.setPreferredSize(new Dimension(300,200));
         CadastroPainel.setLayout(new GridLayout(10, 2));
 
         JLabel label = new JLabel("Nome:");
@@ -44,11 +59,98 @@ public class Cadastro extends JPanel {
 
         JTextField textField4 = new JTextField(30);
         CadastroPainel.add(textField4);
+        
+        JButton button = new JButton("Cadastrar");
+        CadastroPainel.add(button);
+        
+        
+        button.setPreferredSize(new Dimension(2, 5));
+        
 
         CadastroPainel.setVisible(false);
         
 		return CadastroPainel;
         
     }
+	
+	public JPanel listarUsuário () {
+		
+		ListarPainel = new JPanel ();
+		ListarPainel.setPreferredSize(new Dimension(300,200));
+        ListarPainel.setLayout(new GridLayout(10, 2));
+        
+        Object [][]dados = {
+        		{"Laisa", "4437483","9349849", "Engenharia Computacao"}	     		
+        };
+        
+        String [] colunas = {"Nome", "CPF", "Matricula", "Vertente"}	;
+        
+        tabela = new JTable(dados, colunas);
+        ListarPainel.add(tabela);
+        barraRolagem = new JScrollPane(tabela);
+        ListarPainel.add(barraRolagem);
+    
+
+        ListarPainel.setVisible(false);
+        
+		return ListarPainel;
+		
+	}
+	
+	public JPanel AtualizarUsuario () {
+		
+		AtualizarPainel = new JPanel();
+		AtualizarPainel.setPreferredSize(new Dimension(300,200));
+		AtualizarPainel.setLayout((new GridLayout(10,2)));
+		
+		
+		JLabel label = new JLabel("Matrícula:");
+        AtualizarPainel.add(label);
+
+        JTextField textField = new JTextField(30);
+        AtualizarPainel.add(textField);
+
+        JLabel label2 = new JLabel("Vartente:");
+        AtualizarPainel.add(label2);
+
+        JTextField textField2 = new JTextField(11);
+        AtualizarPainel.add(textField2);
+
+        
+        JButton button = new JButton("Atualizar");
+        AtualizarPainel.add(button);
+       
+        button.setPreferredSize(new Dimension(2, 5));
+        
+        AtualizarPainel.setVisible(false);
+    	
+		return AtualizarPainel;
+
+	}
+	
+	public JPanel DeletarUsuario() {
+		
+		DeletarPainel = new JPanel();    
+    	DeletarPainel.setPreferredSize(new Dimension(300,200));
+        DeletarPainel.setLayout(new GridLayout(10, 2));
+
+        JLabel label = new JLabel("Matrícula:");
+        DeletarPainel.add(label);
+
+        JTextField textField = new JTextField(30);
+        DeletarPainel.add(textField);
+	
+        JButton button = new JButton("Deletar");
+        DeletarPainel.add(button);
+       
+        button.setPreferredSize(new Dimension(2, 5));
+        
+        DeletarPainel.setVisible(false);
+    	
+		return DeletarPainel;
+
+	}
+	
+	
 }
 
--- TelaDeLogin/src/Cadastro/Painel.java
+++ TelaDeLogin/src/Cadastro/Painel.java
@@ -1,11 +1,10 @@
 package Cadastro;
 
-import java.awt.Button;
-import java.awt.CardLayout;
 import java.awt.FlowLayout;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+
 import javax.swing.*;
 
 public class Painel extends JPanel {
@@ -15,9 +14,7 @@ public class Painel extends JPanel {
 	private JButton Atualizar;
 	private JButton Deletar;
 	Cadastro cadastro;
-	
-	
-	
+
 	private JButton criarBotao(String nome) {
 		
 		return new JButton(nome);
@@ -37,12 +34,16 @@ public class Painel extends JPanel {
 		
 		this.Listar = this.criarBotao("Listar");
 		this.add(this.Listar);
-
+		this.Listar.addActionListener(new eventoListar());
+		
+		
 		this.Atualizar = this.criarBotao("Atualizar");
 		this.add(this.Atualizar);
+		this.Atualizar.addActionListener(new eventoAtualizar());
 
 		this.Deletar = this.criarBotao("Deletar");
 		this.add(this.Deletar);
+		this.Deletar.addActionListener(new eventoDeletar());
 		
 	}
 	
@@ -50,12 +51,75 @@ public class Painel extends JPanel {
 
 		public void actionPerformed(ActionEvent e) {
 			
-			cadastro.CadastroPainel.setVisible(true);
-			
+			if(cadastro.CadastroPainel.isVisible()) {
+			cadastro.CadastroPainel.setVisible(false);
+			}
+			else {
+				cadastro.CadastroPainel.setVisible(true);
+				cadastro.AtualizarPainel.setVisible(false);
+				cadastro.ListarPainel.setVisible(false);
+				cadastro.DeletarPainel.setVisible(false);
+			}
 			
 		}
 		
 	}
 	
+	public class eventoListar implements ActionListener{
+		
+		public void actionPerformed (ActionEvent e) {
+			
+			if(cadastro.ListarPainel.isVisible()) {
+				cadastro.ListarPainel.setVisible(false);
+			}
+			else {
+				
+				cadastro.CadastroPainel.setVisible(false);
+				cadastro.AtualizarPainel.setVisible(false);
+				cadastro.ListarPainel.setVisible(true);
+				cadastro.DeletarPainel.setVisible(false);
+				
+			}	
+		}
+		
+	}
+	
+	public class eventoAtualizar implements ActionListener{
+		
+		public void actionPerformed (ActionEvent e) {
+			
+		if(cadastro.AtualizarPainel.isVisible()) {
+				cadastro.AtualizarPainel.setVisible(false);
+			}
+			else {
+				
+				cadastro.CadastroPainel.setVisible(false);
+				cadastro.AtualizarPainel.setVisible(true);
+				cadastro.ListarPainel.setVisible(false);
+				cadastro.DeletarPainel.setVisible(false);
+				
+			}		
+		}
+		
+	}
+	
+	public class eventoDeletar implements ActionListener{
+		
+		public void actionPerformed (ActionEvent e) {
+			
+		if(cadastro.DeletarPainel.isVisible()) {
+				cadastro.DeletarPainel.setVisible(false);
+			}
+			else {
+				
+				cadastro.CadastroPainel.setVisible(false);
+				cadastro.AtualizarPainel.setVisible(false);
+				cadastro.ListarPainel.setVisible(false);
+				cadastro.DeletarPainel.setVisible(true);
+				
+			}		
+		}
+		
+	}
 
 }
